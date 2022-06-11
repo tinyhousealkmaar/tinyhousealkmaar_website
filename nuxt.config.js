d5a7a8568b6f9c5746f7a07dc3bfa9fd578b30b0
Ramon de Jezus <ramonakira@live.com>
2022-06-11 16:38:30 +0200

    fix logo color

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extractCSS: true,
+    // extractCSS: true,
     html: {
       minify: {
         minifyCSS: false,
