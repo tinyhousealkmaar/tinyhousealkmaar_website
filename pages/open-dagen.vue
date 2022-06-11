77a46c6aae9bb331affa59fa2664f0fd21dd68db
Ramon de Jezus <ramonakira@live.com>
2022-06-11 16:01:30 +0200

    size for tha 1 image

--- pages/open-dagen.vue
+++ pages/open-dagen.vue
@@ -4,7 +4,7 @@
       <GridMainColumn>
         <Prose>
           <h1>Open dagen</h1>
-          <img src="~/static/images/THA 1.jpg" />
+          <nuxt-picture src="/images/THA 1.jpg" quality="80" width="600" />
 
           <p>
             Onze missie is om de tiny house spirit te promoten. Daarom
